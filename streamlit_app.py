b5992b77dfaae6b5b50fe9bba991cc87fa759e9b
yuzom <146829571+yuzom@users.noreply.github.com>
2024-09-10 16:56:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ with st.expander('Data visualization'):
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-  gender = st.selectbox('Gender', ('male, 'female')
+  gender = st.selectbox('Gender', ('male, 'female'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
   
   
