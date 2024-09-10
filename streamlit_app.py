39821fb6a83a0700318d53ee98b32688ce4bcb9b
yuzom <146829571+yuzom@users.noreply.github.com>
2024-09-10 16:26:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This app bulds a machine learning model')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+df
