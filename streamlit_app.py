56d91de14d04f7f1a427663c6e346a890fc9556b
yuzom <146829571+yuzom@users.noreply.github.com>
2024-09-10 17:05:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,16 +32,19 @@ with st.sidebar:
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
   gender = st.selectbox('Gender', ('male', 'female'))
 
-# Create dataframe for input features
-data = {'island', island,
-        'bill_length_mm', bill_length_mm,
-        'bill_depth_mm', bill_depth_mm,
-        'flipper_length_mm', flipper_length_mm,
-        'body_mass_g', body_mass_g,
-        'gender', gender
-       }
+  # Create dataframe for input features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'gender': gender
+         }
+  
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X], axis=0)
 
-input_df = pd.DataFrame(data, index=[0])
+input_df
 
 
 
