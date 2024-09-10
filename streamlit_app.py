48e6c92bdfe00222890e95e901c6e4524d7d995e
yuzom <146829571+yuzom@users.noreply.github.com>
2024-09-10 17:09:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ with st.sidebar:
           'bill_depth_mm': bill_depth_mm,
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
-          'gender': gender
+          'sex': gender
          }
   
   input_df = pd.DataFrame(data, index=[0])
@@ -49,6 +49,12 @@ with st.expander('Input features'):
   input_df
   st.write('**Combined penguins data**')
   input_penguins
+
+# Encode
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+
 
 
 
